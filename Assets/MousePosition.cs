3eee1c195583dcdef707a396a2d75068c6bfe505
Winnie Song <w.jy.song@gmail.com>
2021-11-10 18:20:52 -0500

    Added scene, sprites and code

---
+++ Assets/MousePosition.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePosition : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get mouse position in pixel coordinates on screen
+        Vector2 mousePositionOnScreen = Input.mousePosition;
+
+        //clamp this number to screenspace
+        mousePositionOnScreen.x = Mathf.Clamp(mousePositionOnScreen.x, 0f, Camera.main.pixelWidth);
+        mousePositionOnScreen.y = Mathf.Clamp(mousePositionOnScreen.y, 0f, Camera.main.pixelHeight);
+
+        //translate this new position to in-world coordinates
+        Vector3 mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePositionOnScreen);
+
+        //move this to that space
+        transform.position = new Vector3 (mousePositionInWorld.x, mousePositionInWorld.y, 0);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    public Transform endTransform;
+
+    public ParticleSystem dirtParticle;
+
+    public Animator myAnimator;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb2D.AddForce(new Vector2(1 * 50f, 0));
+                Debug.Log("MOVE");
+            } else if (Input.GetKey(KeyCode.Space)){
+                Vector3 currentEndPosition = endTransform.position;
+                StartCoroutine(LerpToMouse(currentEndPosition, 1));
+        }
+        didCollideThisFrame = false;
+    }
+
+    IEnumerator LerpToMouse(Vector3 targetPosition, float lerpDuration){
+        float t = 0;
+        Vector3 startPosition = transform.position;
+
+        while (t < lerpDuration){
+            transform.position = Vector3.Lerp(startPosition, targetPosition, t/lerpDuration);
+            t += Time.deltaTime;
+
+            yield return null;
+        }
+        
+        transform.position = targetPosition;
+    }
+
+    private bool didCollideThisFrame;
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            if (didCollideThisFrame) return;
+            if (rb2D.velocity.y > 2){
+                dirtParticle.transform.position = new Vector3(transform.position.x, -3, transform.position.z);
+            dirtParticle.Play();
+
+            myAnimator.Play("Bounce");
+        }
+            didCollideThisFrame = true;
+        }
+    }
+}
